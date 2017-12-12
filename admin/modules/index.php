c949c07b89813a227eb5ff5b1f8823f5b3ba637b
andrew-macpherson <andrewmac17@gmail.com>
2017-12-12 10:52:05 -0500

    theme update

--- admin/modules/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include(TEMPLATEPATH.'/admin/modules/phonak-content/index.php');
-include(TEMPLATEPATH.'/admin/modules/hearing-quiz/index.php');
-include(TEMPLATEPATH.'/admin/modules/hearing-quiz/mailer.php');
